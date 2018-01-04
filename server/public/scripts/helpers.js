daa782f3fefd457ba6af9307586a0c9d751652e2
Stephanie Ratanas <stephanieratanas@gmail.com>
2018-01-03 20:04:32 -0600

    Changed helpers to a service

--- server/public/scripts/helpers.js
+++ server/public/scripts/helpers.js
@@ -1,16 +1,10 @@
-myApp.factory('alert', function($uibModal) {
+myApp.service('alert', function($uibModal) {
   
       function show(action, event) {
         if (action === 'NewEventClicked') {
         return $uibModal.open({
           templateUrl: '/views/templates/newModalContent.html',
-          controller: function() {
-            var vm = this;
-            vm.action = action;
-            vm.event = event;
-        
-          },
-          controllerAs: 'vm'
+          controller: 'ModalController as vm'
         });
         } else {
         return $uibModal.open({
@@ -29,16 +23,40 @@ myApp.factory('alert', function($uibModal) {
         show: show
       };
 
-      vm.saveEventToProfile = function(eventToSave){
-        console.log('save event to profile button clicked');
-        $http({
-            method: 'POST',
-            url: '/events/saveEventToProfile',
-            data: eventToSave
-        }).then(function (response){
-            console.log('response', response);
-            
-        })
-    }
   
 });
+
+
+// myApp.factory('alert', function($uibModal) {
+  
+//       function show(action, event) {
+//         if (action === 'NewEventClicked') {
+//         return $uibModal.open({
+//           templateUrl: '/views/templates/newModalContent.html',
+//           controller: function() {
+//             var vm = this;
+//             vm.action = action;
+//             vm.event = event;
+        
+//           },
+//           controllerAs: 'vm'
+//         });
+//         } else {
+//         return $uibModal.open({
+//           templateUrl: '/views/templates/modalContent.html',
+//           controller: function() {
+//             var vm = this;
+//             vm.action = action;
+//             vm.event = event;
+//           },
+//           controllerAs: 'vm'
+//         });
+//       }
+//       }
+  
+//       return {
+//         show: show
+//       };
+
+  
+// });
